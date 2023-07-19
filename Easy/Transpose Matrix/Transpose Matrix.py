ff68747949d6008680b579b7cbb3da83ad7f683c
Garg <ayushigarg67@gmail.com>
2023-07-19 12:19:04 +0530

    Added Python Solution

--- Easy/Transpose Matrix/Transpose Matrix.py
+++ Easy/Transpose Matrix/Transpose Matrix.py
@@ -1,6 +1,7 @@
 def transposeMatrix(matrix):
-    output = [[] for _ in range(len(matrix[0]))]
+    output = []
     for i in range(len(matrix[0])):
+        output.append([])
         for j in range(len(matrix)):
             output[i].append(matrix[j][i])
     return output
